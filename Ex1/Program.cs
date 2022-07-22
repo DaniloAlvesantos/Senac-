82a38d9b10526c0d87e17a8ac2172234733d9bea
danilo.asantos31 <danilo.asantos31@ITA0557223W10-1>
2022-07-22 20:46:11 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿int a, b, r;
+
+Console.WriteLine("Digite o 1° valor: ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o 2° valor: ");
+b = int.Parse(Console.ReadLine());
+
+r = a + b;
+Console.Clear();
+Console.WriteLine("Resultado: " + r);
---
+++ Hello World/Program.cs
@@ -0,0 +1,8 @@
+﻿string name;
+
+Console.WriteLine("Hello World");
+Console.WriteLine("Digite seu nome: ");
+name = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine("Olá, " + name);
