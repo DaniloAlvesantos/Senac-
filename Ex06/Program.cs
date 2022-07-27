07387cbe00012f6409593f060589cbaaf680b436
DANILO ALVES DOS SANTOS <danilo.asantos31@senacsp.edu.br>
2022-07-26 22:24:23 -0300

    last ex from class

---
+++ Ex06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1, name2, sex1, sex2;
+            Console.WriteLine("Type your name: ");
+            name1 = Console.ReadLine();
+            Console.WriteLine("Type your sex: ");
+            sex1 = Console.ReadLine();
+
+            Console.WriteLine("Type a nother name: ");
+            name2 = Console.ReadLine();
+            Console.WriteLine("Type the sex: ");
+            sex2 = Console.ReadLine();
+
+            Console.WriteLine();
+            if (sex1 == "m" ^ sex2 == "m")
+                Console.WriteLine(name1 + " dence with " + name2);
+            else
+                Console.WriteLine(name1 + " not dance with " + name2);
+        }
+    }
+}
---
+++ Ex07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, x;
+            Console.WriteLine("Type one value: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Type another value: ");
+            b= float.Parse(Console.ReadLine());
+            Console.WriteLine("Type the last value: ");
+            c= float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (!(c > 5))
+                x = (a + b) * c;
+            else
+                x = (b - a) * c;
+            Console.WriteLine("O resultado de X equivale a: " + x);
+        }
+    }
+}
