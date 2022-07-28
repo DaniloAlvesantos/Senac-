6bac9aa7055f9be224a4d305b1216d6d3a7ceb02
DANILO ALVES DOS SANTOS <danilo.asantos31@senacsp.edu.br>
2022-07-27 21:52:01 -0300

    exercises with if´s

---
+++ Ex calculadora/Program.cs
@@ -0,0 +1,37 @@
+﻿int opc;
+float n1, n2, r;
+
+Console.WriteLine("Digite o 1° valor: ");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o 2° valor: ");
+n2 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("[1]..........Adição");
+Console.WriteLine("[2].......Subtração");
+Console.WriteLine("[3]...Multiplicação");
+Console.WriteLine("[4].........Divisão");
+Console.WriteLine();
+Console.WriteLine("Escolha uma opção --> ");
+opc = int.Parse(Console.ReadLine());
+
+if( opc == 1)
+{
+    r = n1 + n2;
+    Console.WriteLine("Resultado = " + r);
+}
+if(opc == 2)
+{
+    r = n1 - n2;
+    Console.WriteLine("Resultado = " + r);
+}
+if( opc == 3)
+{
+    r = n1 * n2;
+    Console.WriteLine("Resultado = " + r);
+}
+if(opc == 4)
+{
+    r = n1 / n2;
+    Console.WriteLine("Resultado = " + r);
+}
+
---
+++ Ex triangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿float val1, val2, val3;
+
+Console.WriteLine("Digite o 1º valor do triangulo: ");
+val1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o 2º valor do triangulo");
+val2 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o 3ºvalor do triangulo");
+val3 = float.Parse(Console.ReadLine());
+
+Console.Clear();
+
+if((val1 + val2) < val3 || (val3 + val1) < val2 || (val2 + val3) < val1) 
+{
+    Console.WriteLine("Não é um triangulo");
+}
+else
+{
+    Console.WriteLine();
+    if(val1 == val2 && val3 == val1)
+    {
+        Console.WriteLine("Triangulo Equilátero");
+    } else if(val1 == val2 || val3 == val2 || val3 == val1)
+    {
+        Console.WriteLine("Triangulo Isósceles");
+    } else
+    {
+        Console.WriteLine("Triangulo Escaleno");
+    }
+}
+
