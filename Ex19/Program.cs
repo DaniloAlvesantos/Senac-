0c7f98213e31b0b015bf27f14caaaceb90ebe6d0
DANILO ALVES DOS SANTOS <danilo.asantos31@senacsp.edu.br>
2022-07-29 21:38:20 -0300

    Formatation

--- Ex19/Program.cs
+++ Ex19/Program.cs
@@ -1,7 +1,6 @@
 ﻿/*
     Programa: Formatação
  */
-
 double v1 = 123.456,
     v2 = 234.5,
     v3 = 345.0,
@@ -14,30 +13,23 @@ int v7 = 123,
 long x = 1234567890, 
     y = 12345678901;
 
-Console.WriteLine(String.Format("{0:0.00}", v1)); // Dinheiro
-Console.WriteLine(String.Format("{0:0.00}", v2)); // Dinheiro
-Console.WriteLine(String.Format("{0:0.00}", v3)); // Dinheiro
-Console.WriteLine(String.Format("{0:0,0.00}", v4)); // Dinheiro
-Console.WriteLine(String.Format("{0:0,0.00}", v5)); // Dinheiro
-Console.WriteLine(String.Format("{0:0,0.00}", v6)); // Dinheiro
-Console.WriteLine(String.Format("{0,15:0,0.00}", v4)); // Max 15 casas
-Console.WriteLine(String.Format("{0,15:0,0.00}", v5)); // Max 15 casas
-Console.WriteLine(String.Format("{0,15:0,0.00}", v2)); // Max 15 casas
+Console.WriteLine(String.Format("{0:0.00}", v1)); // valores milhar 2 casas decimais
+Console.WriteLine(String.Format("{0:0.00}", v2)); // valores milhar 2 casas decimais
+Console.WriteLine(String.Format("{0:0.00}", v3)); // valores milhar 2 casas decimais
+Console.WriteLine(String.Format("{0:0,0.00}", v4)); // valores milhar 2 casas decimais
+Console.WriteLine(String.Format("{0:0,0.00}", v5)); // valores milhar 2 casas decimais
+Console.WriteLine(String.Format("{0:0,0.00}", v6)); // valores milhar 2 casas decimais
+Console.WriteLine(String.Format("{0,15:0,0.00}", v4)); // valores milhar Max 15 casas
+Console.WriteLine(String.Format("{0,15:0,0.00}", v5)); // valores milhar Max 15 casas
+Console.WriteLine(String.Format("{0,15:0,0.00}", v2)); // valores milhar Max 15 casas
+Console.WriteLine(String.Format("{0:00000}", v7)); // casas vazias completas por 0
+Console.WriteLine(String.Format("{0:00000}", v8)); // casas vazias completas por 0
+Console.WriteLine(String.Format("{0:00000}", v9)); // casas vazias completas por 0
 Console.WriteLine(String.Format("{0,5}", v7)); // max 5 casas
 Console.WriteLine(String.Format("{0,5}", v8)); // max 5 casas
 Console.WriteLine(String.Format("{0,5}", v9)); // max 5 casas
 Console.WriteLine(String.Format("{0:(0xx##)####-####}", x)); // Telefone
 Console.WriteLine(String.Format(@"{0:###\.###\.###-###}", y)); // CPF
-
-    string r = "9";
-    int n = 4;
-    for (int i = 1; i < n; i++)
-    {
-        r += String.Format("{0}", r);
-    }
-    Console.WriteLine(int.Parse(r));
-
-
 Console.WriteLine();
 Console.WriteLine("Tecle algo para encerrar... ");
 Console.ReadKey();
