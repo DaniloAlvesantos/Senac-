46c8b7be322aa07f029412a071cc0de790f7f620
DANILO ALVES DOS SANTOS <danilo.asantos31@senacsp.edu.br>
2022-08-01 22:21:08 -0300

    last exercise from class

--- Ex26/Program.cs
+++ Ex26/Program.cs
@@ -1,14 +1,14 @@
-﻿Console.WriteLine(Math.Acos(-1));
-Console.WriteLine(Math.Asin(-1));
-Console.WriteLine(Math.Atan(1));
-Console.WriteLine(Math.Sin(1));
-Console.WriteLine(Math.Tan(4));
-Console.WriteLine(Math.Pow(2, 3));
-Console.WriteLine(Math.Sqrt(9.0));
-Console.WriteLine(Math.Exp(1));
-Console.WriteLine(Math.Log(3));
-Console.WriteLine(Math.Abs(-9));
-Console.WriteLine(Math.Floor(2.03));
-Console.WriteLine(Math.Max(10,3));
-Console.WriteLine(Math.Min(10,3));
-Console.WriteLine(Math.Round(5.8));
+﻿Console.WriteLine(Math.Acos(-1)); //3,141592653589793
+Console.WriteLine(Math.Asin(-1)); //-1,5707963267948966
+Console.WriteLine(Math.Atan(1)); //0,7853981633974483
+Console.WriteLine(Math.Sin(1)); //0,8414709848078965
+Console.WriteLine(Math.Tan(4)); //1,1578212823495777
+Console.WriteLine(Math.Pow(2, 3)); // 8
+Console.WriteLine(Math.Sqrt(9.0)); // 3
+Console.WriteLine(Math.Exp(1)); //2,718281828459045
+Console.WriteLine(Math.Log(3)); //1,0986122886681098
+Console.WriteLine(Math.Abs(-9)); //9
+Console.WriteLine(Math.Floor(2.03)); //2
+Console.WriteLine(Math.Max(10,3)); // 10
+Console.WriteLine(Math.Min(10,3)); // 3
+Console.WriteLine(Math.Round(5.8)); //6
---
+++ Ex27/Program.cs
@@ -0,0 +1,9 @@
+﻿string texto = "Computador", n = "danilo";
+
+Console.WriteLine(texto.Length);
+Console.WriteLine(texto.ToLower());
+Console.WriteLine(texto.ToUpper());
+Console.WriteLine(texto.Substring(0,3));
+Console.WriteLine(texto.Substring(3,4));
+Console.WriteLine(texto.Substring(7,3));
+Console.WriteLine(n.Substring(2,4));
