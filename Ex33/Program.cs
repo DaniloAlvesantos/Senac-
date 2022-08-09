4fd9b43d8e12b0782cfb03113a9ff60f508eb1a4
DANILO ALVES DOS SANTOS <danilo.asantos31@senacsp.edu.br>
2022-08-08 21:50:41 -0300

    OOB

---
+++ Ex33/Program.cs
@@ -0,0 +1,127 @@
+﻿class program {
+    public static float r, a, b;
+    public static string MSG = "Tecle algo para continuar...";
+    static void Main(String[] args)
+    { 
+        string opcao;
+        opcao = "0";
+        while (opcao != "5")
+        {
+            Console.Title = "Calculadora";
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("Main menu");
+            Console.WriteLine();
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
+            Console.WriteLine();
+            Console.WriteLine("Escolha a opção");
+            opcao = Console.ReadLine();
+            if (opcao != "5")
+            {
+                switch (opcao)
+                {
+                    case "1":
+                        RotSoma();
+                        break;
+                    case "2":
+                        RotSubtracao();
+                        break;
+                    case "3":
+                        RotMultiplicacao();
+                        break;
+                    case "4":
+                        RotDivisao();
+                        break;
+                    default:
+                        Console.WriteLine("valor inválido");
+                        Console.WriteLine(MSG);
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+    }
+    private static void RotDivisao()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Rotina de Divisão");
+        Console.WriteLine();
+        Entrada();
+        if (b == 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Erro");
+            Console.WriteLine(MSG);
+            Console.ReadKey();
+        }
+        else
+        {
+            r = Calculo(a, b, "/");
+            Saida();
+        }
+    }
+    private static void RotMultiplicacao()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Rotina de Multiplicação");
+        Console.WriteLine();
+        Entrada();
+        r = Calculo(a, b, "*");
+        Saida();
+    }
+    private static void RotSoma()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Rotina de Soma");
+        Console.WriteLine();
+        Entrada();
+        r = Calculo(a, b, "+");
+        Saida();
+    }
+    private static void RotSubtracao()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Subtração");
+        Console.WriteLine();
+        Entrada();
+        r = Calculo(a, b, "-");
+        Saida();
+    }
+    public static float Calculo(float x, float y, string operador)
+    {
+        float z = 0;
+        switch (operador)
+        {
+            case "+": z = x + y; break;
+            case "-": z = y - x; break;
+            case "*": z = x * y; break;
+            case "/": z = x / y; break;
+        }
+        return z;
+    }
+    private static void Entrada()
+    {
+        Console.Write("Entre um valor para A:");
+        a = float.Parse(Console.ReadLine());
+        Console.WriteLine("Entre um valor para B:");
+        b = float.Parse(Console.ReadLine());
+    }
+    private static void Saida()
+    {
+        Console.WriteLine();
+        Console.WriteLine("O resultado entre A e B = {0:0.00}", r);
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine(MSG);
+        Console.ReadKey();
+    }
+}
---
+++ teste/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace teste
+{
+    internal class Program
+    {
+        public class animal
+        {
+            public string nome { get; }
+            public string raca { get; }
+            public string color { get; }
+            public int age { get; }
+            public int val = 0;
+            public animal(string v1, string v2, string v3, int v4)
+            {
+                nome = v1;
+                raca = v2;
+                color = v3;
+                age = v4;
+
+            }
+            public void about()
+            {
+                Console.WriteLine($"Nome:{this.nome}");
+                Console.WriteLine($"Raça:{this.raca}");
+                Console.WriteLine($"Cor: {this.color}");
+                Console.WriteLine($"Idade: {this.age}");
+            }
+            public int Play()
+            {
+                int x = 0;
+                Console.WriteLine("Digite quantos minutos ira brincar com o cachorro");
+                x = int.Parse(Console.ReadLine());
+                if(x == 0)
+                    return val;
+                else
+                    return val += x;
+            }
+            public void Status()
+            {
+                switch (val)
+                {
+                    case 0:
+                        Console.WriteLine($"The {this.nome} is borring");
+                        break;
+                    case 1:
+                        Console.WriteLine($"The {this.nome} wants to play more!!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Try more");
+                        break;
+                    case 3:
+                        Console.WriteLine($"The {this.nome} is happy");
+                        break;
+                    case 4:
+                        Console.WriteLine($"The {this.nome} cant stop to play now");
+                        break;
+                    case 5:
+                        Console.WriteLine($"The {this.nome} is tiring");
+                        break;
+                    default:
+                        Console.WriteLine($"{this.nome} is sleeping...");
+                        break;
+                }
+            }
+        }
+        class Secon
+        {
+            static string n, r, c;
+            static int a;
+            private static void forms()
+            { 
+                Console.WriteLine("Digite o nome do seu animal: ");
+                n = Console.ReadLine();
+                Console.WriteLine("Digite a raça: ");
+                r = Console.ReadLine();
+                Console.WriteLine("Digite a cor: ");
+                c = Console.ReadLine();
+                Console.WriteLine("Digite a idade dele: ");
+                a = int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+            {
+                forms();
+                var dog = new animal(n, r, c, a);
+                Console.Clear();
+                Console.WriteLine("Main menu");
+                Console.WriteLine("1 - status");
+                Console.WriteLine("2 - brincar");
+                Console.WriteLine("3 - informações");
+                Console.WriteLine("4 - Sair");
+                dog.about();
+                dog.Play();
+                dog.Status();
+            }
+        }
+}
+}
